c63050cdbc515c5586d6f86d7768a9cb67fdedb7
videososo <a12114aa@gazeta.pl>
2025-02-26 15:04:55 +0800

    Add files via upload

---
+++ admin/inc.php
@@ -0,0 +1,5 @@
+<title>爱客影视CMS管理后台 - Powered by 智云</title>
+<meta name="keywords" content="智卓星网开发组" />
+<meta name="description" content="智云影视，苹果CMS程序" />
+<link href="./images/woaik.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico">
